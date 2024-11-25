b1ee21e7ad8b3377de584ddb680df8bbccec1bbe
Nikilandgelo <niki_landgelo@outlook.com>
2024-11-25 12:57:40 +0300

    reformatted because of upgraded ruff checks + sort imports and added type hints + docstrings

--- first_linked_list/linked_list.py
+++ first_linked_list/linked_list.py
@@ -1,28 +1,36 @@
-from object_list import ObjList
-from decorators import non_empty
+"""Implementation of a singly linked list."""
+
+from utils import raise_error_if_not_instance
+
+from .decorators import non_empty
+from .object_list import ObjList
 
 
 class LinkedList:
-    __slots__ = ("head", "tail")
+    """Represents a singly linked list."""
 
-    def __init__(self):
+    __slots__ = "head", "tail"
+
+    def __init__(self) -> None:
+        """Initialize an empty LinkedList."""
         self.__set_default_state()
 
-    def __set_default_state(self):
+    def __set_default_state(self) -> None:
+        """Reset the LinkedList to an empty state."""
         self.head = None
         self.tail = None
 
-    def add_obj(self, obj: ObjList):
-        """Adds an object to the end of the linked list.
+    def add_obj(self, obj: ObjList) -> None:
+        """Add an object to the end of the linked list.
 
         Args:
-            obj: The object to add.  Must be an instance of ObjList.
+            obj: The object to add. Must be an instance of ObjList.
 
         Raises:
             TypeError: If obj is not an instance of ObjList.
+
         """
-        if not isinstance(obj, ObjList):
-            raise TypeError("Passing object must be ObjList instance")
+        raise_error_if_not_instance([(obj, ObjList)])
         if self.head is None:
             self.head: ObjList = obj
         else:
@@ -31,13 +39,12 @@ class LinkedList:
         self.tail: ObjList = obj
 
     @non_empty
-    def remove_obj(self):
-        """Removes the last object from the linked list.
+    def remove_obj(self) -> None:
+        """Remove the last object from the linked list.
 
-        If the linked list contain only one object, it sets linked list to the
-        initial state - head and tail are None. Otherwise, it updates the
-        tail to point to the previous object, and sets the next pointer of
-        the previous object to None.
+        If the list has only one object, reset it to the empty state.
+        Otherwise, update the tail and adjust the previous object's next
+        pointer.
         """
         prev_obj: ObjList | None = self.tail.get_prev()
         if prev_obj is None:
@@ -48,14 +55,13 @@ class LinkedList:
 
     @non_empty
     def get_data(self) -> list:
-        """Retrieves all data from the linked list as a list.
+        """Retrieve all data from the linked list as a list.
 
-        Traverses the linked list from head to tail, collecting the data
-        from each ObjList object into a list which is then returned.
+        Traverse the list, collecting data from each ObjList object.
 
         Returns:
-            A list containing the data from each ObjList object in the linked
-            list.
+            A list containing the data from all ObjList objects.
+
         """
         all_data: list = []
         current_obj: ObjList = self.head
