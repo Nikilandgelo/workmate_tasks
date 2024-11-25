2084bcaf4a52762b3419c44e411d51c312078f29
Nikilandgelo <niki_landgelo@outlook.com>
2024-11-25 16:21:21 +0300

    added new exceptions, new ignored errors in ruff conf file and reformatted utils module to fit all requirements

--- utils.py
+++ utils.py
@@ -1,10 +1,22 @@
+"""Provides utility functions for type checking."""
+
+# needed because list type hint not < 3.9 compatible
+from __future__ import annotations
+
+from exceptions import WrongTypeError
+
+
 def raise_error_if_not_instance(instances_with_classes: list[tuple]) -> None:
+    """Raise a TypeError if any instance is not an instance of expected class.
+
+    Args:
+        instances_with_classes: A list of tuples, where each tuple contains an
+                                instance and its expected class.
+
+    Raises:
+        TypeError: If any instance is not an instance of its expected class.
+
+    """
     for instance in instances_with_classes:
         if not isinstance(instance[0], instance[1]):
-            raise TypeError(
-                (
-                    f'"{instance[0]}" it`s an instance of '
-                    f'"{instance[0].__class__.__name__}" class, but '
-                    f'"{instance[1].__name__}" class expected.'
-                )
-            )
+            raise WrongTypeError(instance)
