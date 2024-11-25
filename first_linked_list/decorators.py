d727b991c104734a6f42293a29585471b3530798
Nikilandgelo <niki_landgelo@outlook.com>
2024-11-25 12:57:40 +0300

    reformatted because of upgraded ruff checks + sort imports and added type hints + docstrings

--- first_linked_list/decorators.py
+++ first_linked_list/decorators.py
@@ -1,30 +1,34 @@
-from functools import wraps
-from typing import TYPE_CHECKING
+"""Decorators for LinkedList methods."""
 
+from functools import wraps
+from typing import TYPE_CHECKING, Any, Callable
+
+from exceptions import EmptyLinkedListError
 
 if TYPE_CHECKING:
     from linked_list import LinkedList
 
 
-def non_empty(method):
-    """
-    Decorator to ensure that a LinkedList method is called only if the list is
-    not empty.
+def non_empty(method: Callable[..., Any]) -> Callable[..., Any]:
+    """Ensure that a LinkedList method is called only if the list is not empty.
 
     Args:
         method: The LinkedList method to be decorated.
+
+    Raises:
+        ValueError: If the LinkedList is empty.
+
+    Returns:
+        The decorated method.
+
     """
 
     @wraps(method)
-    def wrapper(*args, **kwargs):
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        """Wrap the decorated method."""
         self: LinkedList = args[0]
         if self.tail is None:
-            raise ValueError(
-                (
-                    f'Was called method "{method.__name__}" but '
-                    "LinkedList is empty, so we are returning..."
-                )
-            )
+            raise EmptyLinkedListError(method.__name__)
         return method(*args, **kwargs)
 
     return wrapper
--- first_linked_list/object_list.py
+++ first_linked_list/object_list.py
@@ -1,28 +1,44 @@
-from __future__ import annotations
+"""Implementation of an object within the linked list."""
+
+from typing import Self, Union
 
 
 class ObjList:
-    __slots__ = ("__data", "__next", "__prev")
+    """Represents a single object within the linked list."""
 
-    def __init__(self, data: str):
+    __slots__ = "__data", "__next", "__prev"
+
+    def __init__(self, data: str) -> None:
+        """Initialize an ObjList object with the given data.
+
+        Args:
+            data: The data to store in this object.
+
+        """
         self.__data: str = data
-        self.__next: ObjList | None = None
-        self.__prev: ObjList | None = None
+        self.__next: Self | None = None
+        self.__prev: Self | None = None
 
     def get_data(self) -> str:
+        """Retrieve the data stored in this object."""
         return self.__data
 
     def set_data(self, data: str) -> None:
-        self.__data: str = data
+        """Update the data stored in this object."""
+        self.__data = data
 
-    def get_next(self) -> ObjList | None:
+    def get_next(self) -> Union["Self", None]:
+        """Retrieve the next object in the list."""
         return self.__next
 
-    def set_next(self, obj: ObjList | None) -> None:
-        self.__next: ObjList | None = obj
+    def set_next(self, obj: Union["Self", None]) -> None:
+        """Set the next object in the list."""
+        self.__next = obj
 
-    def get_prev(self) -> ObjList | None:
+    def get_prev(self) -> Union["Self", None]:
+        """Retrieve the previous object in the list."""
         return self.__prev
 
-    def set_prev(self, obj: ObjList | None) -> None:
-        self.__prev: ObjList | None = obj
+    def set_prev(self, obj: Union["Self", None]) -> None:
+        """Set the previous object in the list."""
+        self.__prev = obj
